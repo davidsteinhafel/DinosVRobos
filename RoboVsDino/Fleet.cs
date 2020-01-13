619fd5fdf794df9d4debbb4539e3e327424dc1f6
David steinhafel <dasteinhafel@gmail.com>
2020-01-13 09:01:51 -0600

    double checking project, minor value changes

--- RoboVsDino/Fleet.cs
+++ RoboVsDino/Fleet.cs
@@ -10,11 +10,12 @@ namespace RoboVsDino
     {
         public List<Robot> robots = new List<Robot>();
 
+
         public Fleet()
         {
-            Robot robot1 = new Robot("Vaporizor4200", 7000, 6800, new Weapon(4200, "Rifle"));
-            Robot robot2 = new Robot("Dunkmaster6900", 6500, 1000, new Weapon(6900, "Shotgun"));
-            Robot robot3 = new Robot("Clapper1337", 4300, 8000, new Weapon(1337, "Sub-Machine Gun"));
+            Robot robot1 = new Robot("Vaporizor4200", 7000, 12000, new Weapon(4200, "Rifle"));
+            Robot robot2 = new Robot("Dunkmaster6900", 7200, 12500, new Weapon(6900, "Shotgun"));
+            Robot robot3 = new Robot("Clapper1337", 8800, 13000, new Weapon(1337, "Sub-Machine Gun"));
             robots.Add(robot1);
             robots.Add(robot2);
             robots.Add(robot3);
